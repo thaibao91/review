c60aeda0746d8f9f3d3be6be5572b0cf217f1d9b
LY THAI BAO <thaibao91@gmail.com>
2022-04-05 17:43:07 +0800

    1st version

---
+++ src/main/java/com/LTB/jwdnd/c1/review/MessageService.java
@@ -0,0 +1,27 @@
+package com.LTB.jwdnd.c1.review;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class MessageService {
+    private String message;
+
+    public MessageService(String message) {
+        this.message = message;
+    }
+
+    public String uppercase() {
+        return this.message.toUpperCase();
+    }
+
+    public String lowercase() {
+        return this.message.toLowerCase();
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("Creating MessageService bean");
+    }
+}
---
+++ src/main/java/com/LTB/jwdnd/c1/review/ReviewApplication.java
@@ -0,0 +1,20 @@
+package com.LTB.jwdnd.c1.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+	@Bean
+	public String message() {
+		System.out.println("Creating message bean");
+		return "Hello, Spring!";
+	}
+
+}
